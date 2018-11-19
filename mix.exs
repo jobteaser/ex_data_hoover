c7e1dfe9965576d193a5aaf60222bacce96ad177
Yann VERY <yann.very@gmail.com>
2018-11-19 18:45:49 +0100

    Doc: Add a package description

--- mix.exs
+++ mix.exs
@@ -7,6 +7,7 @@ defmodule ExDataHoover.MixProject do
       version: "0.1.0",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
+      description: "Simple API to implement event sourcing",
       deps: deps(),
       package: package()
     ]
